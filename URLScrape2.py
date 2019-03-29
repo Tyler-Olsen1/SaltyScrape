72e64cd6ce66eb6562db510bf4b146b5dd370ea0
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-03-29 09:53:50 -0600

    Success!!! The fetch worked!!!

--- URLScrape2.py
+++ URLScrape2.py
@@ -3,24 +3,25 @@ from bs4 import BeautifulSoup, SoupStrainer
 from csv import reader
 import csv
 import urllib
+import numpy as np
 
-with open("../ExtractedNumbers.csv", newline='') as csvfile:
-    ExtractedNumbers = csv.reader(csvfile, delimiter='\t', quotechar='|')
-    for row in ExtractedNumbers:
-        str(row)
-        # print(row[1:9])
-        # print(row[0])
+with open("../SerialNumbers.csv", newline='') as csvfile:
+    SerialNumbers = csv.reader(csvfile)
+    for row in SerialNumbers:
+        row = np.array(row)
+        print(row)
 
-serialnumber=row[1:9]
-
+serialnumber=int(row)
+# newSerialNumber=serialnumber[:2]+serialnumber[10:]
+# print(newSerialNumber)
 args = {"av_serial": serialnumber}
 # url = "http://127.0.0.1:5000/data?{}".format(urllib.urlencode(args))
 URL = "http://www.utahcounty.gov/LandRecords/Property.asp?{}".format(urllib.parse.urlencode(args))
 response = requests.get(URL)
-print(response)
 print(URL)
+print(response)
 
 only_td_tags = SoupStrainer("td")
 soup = BeautifulSoup(response.text, "html.parser", parse_only=only_td_tags)
 targetCell = soup.find(text="Mailing Address:")
-# print (targetCell.parent.parent.text)
+print (targetCell.parent.parent.text)
