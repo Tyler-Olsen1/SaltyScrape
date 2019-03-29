c2f8d932771d626d722018c18e6a9f41d9bbffa6
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-03-29 16:38:28 -0600

    I'm back to square one. Will copy and try to go forward on the copy.

--- URLScrape3.py
+++ URLScrape3.py
@@ -1,44 +1,17 @@
 import requests
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup#, SoupStrainer
 from csv import reader
 import csv
 import urllib
-import ast
 import numpy as np
+import pandas as pd
 import asyncio
 
 
-with open("../SerialNumbers.csv", newline='') as csvfile:
-    SerialNumbers = csv.reader(csvfile)
-    for row in SerialNumbers:
-        array = np.array(row)
-        # print(array)
+page = requests.get('http://www.utahcounty.gov/LandRecords/Property.asp?av_serial=20160026')
 
-for i in range(SerialNumbers):
-    [float(i) for i in SerialNumbers]
-    SerialNumbers[i] = int(SerialNumbers[i])
-args = {"av_serial": i}
+soup = BeautifulSoup(page.text, 'html.parser')
+only_td_tags = soup.find("td")
+targetCell = soup.find(text="Mailing Address:")
+print (targetCell.parent.parent.text)
 
-# def arrayArgs():
-#     print(args)
-
-def literal_eval_to_array(SerialNumbers):
-    return np.array(ast.literal_eval(SerialNumbers))
-
-# async def arrayI():
-#     await arrayArgs() 
-
-def response():
-    requests.get(URL)
-    print(URL)
-
-async def URL():
-    URL = "http://www.utahcounty.gov/LandRecords/Property.asp?{}".format(urllib.parse.urlencode(args))
-    await response()
-    #     requests.get(URL)
-    # print(URL)
-    print(response)
-    only_td_tags = SoupStrainer("td")
-    soup = BeautifulSoup(response().text, "html.parser", parse_only=only_td_tags)
-    targetCell = soup.find(text="Mailing Address:")
-    print (targetCell.parent.parent.text)
